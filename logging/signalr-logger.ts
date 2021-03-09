6bdfee0a583fbd53d9b84d015501d458b4f8b9d1
Sebby <lipmas93@gmail.com>
2021-03-09 10:40:01 -0500

    Fix identation to use 4 spaces

--- logging/signalr-logger.ts
+++ logging/signalr-logger.ts
@@ -12,28 +12,28 @@ export class SignalRLogger implements signalR.ILogger
 
     log(logLevel: signalR.LogLevel, message: string): void {
         switch(logLevel) {
-            case LogLevel.Trace:
-                this.logger.trace(message);
-                break;
-            case LogLevel.Debug:
-                this.logger.debug(message);
-                break;
-            case LogLevel.Information:
-                this.logger.info(message);
-                break;
-            case LogLevel.Warning:
-                this.logger.warn(message);
-                break;
-            case LogLevel.Error:
-                this.logger.error(message);
-                break;
-            case LogLevel.Critical:
-                this.logger.error(message);
-                break;
-            case LogLevel.None:
-                break;
-            default:
-                throw new Error('Unhandled signal log level');
+        case LogLevel.Trace:
+            this.logger.trace(message);
+            break;
+        case LogLevel.Debug:
+            this.logger.debug(message);
+            break;
+        case LogLevel.Information:
+            this.logger.info(message);
+            break;
+        case LogLevel.Warning:
+            this.logger.warn(message);
+            break;
+        case LogLevel.Error:
+            this.logger.error(message);
+            break;
+        case LogLevel.Critical:
+            this.logger.error(message);
+            break;
+        case LogLevel.None:
+            break;
+        default:
+            throw new Error('Unhandled signal log level');
         }
     }
 }
